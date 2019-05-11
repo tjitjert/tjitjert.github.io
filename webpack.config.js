228c62702acba5af857b082f9627eac193973d97
Karin Keijzer <karin.keijzer@capgemini.com>
2019-05-11 14:15:38 +0200

    Added countdown of 3 sec, add font, fix score undefined, updated show/hide functionality.

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: './src/js/main.js',
-  mode:'development',
+  mode: 'development',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'docs')
@@ -18,12 +18,13 @@ module.exports = {
   },
   plugins: [
     new CopyWebpackPlugin([
-        {from: './src/html/**/*', to:'', flatten: true},
-        {from: './src/css/**/*', to:'', flatten: true},
-        {from: './vendor/**/*', to:'', flatten: true},
-        {from: './assets/img/**/*', to: ''},
-        {from: './assets/audio/**/*', to: ''},
-        {from: './assets/levels/**/*', to: ''}
+      {from: './src/html/**/*', to: '', flatten: true},
+      {from: './src/css/**/*', to: '', flatten: true},
+      {from: './vendor/**/*', to: '', flatten: true},
+      {from: './assets/audio/**/*', to: ''},
+      {from: './assets/fonts/**/*', to: ''},
+      {from: './assets/img/**/*', to: ''},
+      {from: './assets/levels/**/*', to: ''}
     ])
   ]
 };
