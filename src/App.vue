c87abeb4741d7f6456fd972cb4468248296fe528
tjitjert <tjitjert@example.com>
2020-12-18 20:16:59 +0100

    gedicht

--- src/App.vue
+++ src/App.vue
@@ -22,10 +22,173 @@
       <div v-if="section === 'gedicht'" id="gedicht" class="p-3">
         <h3 class="text-secondary">Gedicht</h3>
         <p>
-          Planten zijn je lust en je leven <br/>
-          De Sint heeft ze je vaak gegeven <br/>
-          Je huis moet wel een rimboe zijn <br/>
-          Maar dat vind jij juist fijn
+          Joho Eva,
+        </p>
+        <p>
+          Zit je klaar?<br />
+          Want de tijd is weer daar.<br />
+          Voor dit poëtische gebaar.<br />
+        </p>
+        <p>
+          Het is vjezze klaas.<br />
+          En zijn pieter baas.<br />
+          Rappend in een raas.<br />
+        </p>
+        <p>
+          us bereid je voor.<br />
+          Op dit rap symfonie koor.<br />
+          Direct in je oor.<br />
+        </p>
+        <p>
+          Wat een jaar.<br />
+          Een beetje raar.<br />
+          Maar gelukkig niet allemaal naar.<br />
+        </p>
+        <p>
+          Samenwonen in grunn.<br />
+          Samen met P fabergé, wat een fun.<br />
+          Of reikte je al snel voor je gun?<br />
+        </p>
+        <p>
+          Was het even wennen?<br />
+          Je leert wel alles van elkaar kennen.<br />
+          Bevalt het toch niet, kan je altijd nog wegrennen.<br />
+        </p>
+        <p>
+          Helemaal met zo'n speciaal geval.<br />
+          Die fabergé is soms een beetje mal.<br />
+          Maar stukken beter dan zo'n glibberige vindicat bal.<br />
+        </p>
+        <p>
+          Volgens mij is het een pot blijheid.<br />
+          Elkaar te hebben in dichtere nabijheid.<br />
+          Bestrijkt met een goede laag vrijheid.<br />
+        </p>
+        <p>
+          Samen bouwend aan jullie eigen tuin.<br />
+          Tussen het KPN parkeerplaats puin.<br />
+          Zelfgeknutselde bakken vol met aardebruin.<br />
+        </p>
+        <p>
+          Hopend op groeiend groen.<br />
+          Mooie planten en een kruiden legioen.<br />
+          Dat stukje natuur, is waar jullie het voor doen.<br />
+        </p>
+        <p>
+          We zijn ontzettend benieuwd naar je grote project.<br />
+          Een Eva original, de ekte ekt.<br />
+          Ben je al bijna klaar met de laatste act?<br />
+        </p>
+        <p>
+          Met 2 fantastische oude vrouwen op pad.<br />
+          Waarvan 1 soms iets vergat.<br />
+          Volgens mij raakte je dat avontuur niet zat.<br />
+        </p>
+        <p>
+          In vrijwel je eentje zet je zo'n mega project op pote.<br />
+          En we hebben nu al genote.<br />
+          Van menig anekdote.<br />
+        </p>
+        <p>
+          We hebben enkel lof.<br />
+          Voor jou als film prof.<br />
+          De films die je maakt zijn altijd tof.<br />
+        </p>
+        <p>
+          Over naar jullie portugese avontuur.<br />
+          Bijna geblockt door die corona muur.<br />
+          De cursus (s)permacultuur.<br />
+        </p>
+        <p>
+          Het klinkt heel koel.<br />
+          Een groene, ecologische en ethieke boel.<br />
+          Met een mooi geformuleerd doel.<br />
+        </p>
+        <p>
+          Terug naar de natuur.<br />
+          Naar een ontwerpmethode, naakt en puur.<br />
+          Verantwoordelijk voor je eigen vuur.<br />
+        </p>
+        <p>
+          Was het weer terug naar het begin?<br />
+          In levensstijlachtige zin?<br />
+          Op zoek naar de innerlijke groene godin.<br />
+        </p>
+        <p>
+          In plaats van een bad chillen in een gietijzeren teil.<br />
+          Met zeep niet gemaakt van bevergeil.<br />
+          Daardoor fabergé's gezicht vol met kwijl.<br />
+        </p>
+        <p>
+          De surprise is dit jaar een beetje hybride.<br />
+          En kan misschien lijken als een beetje timide.<br />
+          Het is zeker geen egyptische pyramide.<br />
+        </p>
+        <p>
+          Dit gedicht is stap één.<br />
+          Maar dat is voor surprise vrij algemeen.<br />
+          Gelukkig zijn we er bijna doorheen.<br />
+        </p>
+        <p>
+          Stap twee is een instructie.<br />
+          Voor een mini permacultuur productie.<br />
+          Al gaat het enkel om een introductie.<br />
+        </p>
+        <p>
+          In je pakketje vind je een aantal dingen.<br />
+          Waarmee je een ecosysteem in elkaar kan wringen.<br />
+          Met een beetje liefde gaat je creatie al snel zingen.<br />
+        </p>
+        <p>
+          Er mist nog wel wat.<br />
+          Iets vochtigs en nat.<br />
+          En dan heb ik het niet over een bevergeil gevuld bad.<br />
+        </p>
+        <p>
+          Het gaat om een stuk mos.<br />
+          Dus je moet nog even naar het bos.<br />
+          Daar ligt vast nog wel een stukje los.<br />
+        </p>
+        <p>
+          Want deel van een prachtige permacultuur.<br />
+          Hoort natuurlijk een goede hoeveelheid avontuur.<br />
+          En mos blijft wekenlang in een doos niet zo puur.<br />
+        </p>
+        <p>
+          Want deel van een prachtige permacultuur.<br />
+          Hoort natuurlijk een goede hoeveelheid avontuur.<br />
+          En mos blijft wekenlang in een doos niet zo puur.<br />
+        </p>
+        <p>
+          Pak je pakketje maar uit en klik dan hier op 'instructie'.<br />
+          Geef vervolgens ons een introductie.<br />
+          Over ecosysteem productie.<br />
+        </p>
+        <p>
+          Bewaar het bouwen zelf maar voor later.<br />
+          Voor een dag zonder kater.<br />
+          Dan ben je wat accurater.<br />
+        </p>
+        <p>
+          Na de instructie is het tijd voor de knop 'cadeau'.<br />
+          En geloof me, daar ben je zo.<br />
+          Want je zit al lekker in de lezende flow.<br />
+        </p>
+        <p>
+          We zijn het gedicht nu wel zat.<br />
+          Hopelijk vond je het wat.<br />
+          Want elk jaar ligt hij weer wat hoger, die lat.<br />
+        </p>
+        <p>
+          Dit is het einde van deze poetische zee.<br />
+          Tijd voor stap twee.<br />
+          We luisteren met smart met je mee.<br />
+        </p>
+        <p>
+          Liefs,
+        </p>
+        <p>
+          Vjezze klaas en pieter baas
         </p>
       </div>
       <div v-if="section === 'instructie'" id="instructie"
